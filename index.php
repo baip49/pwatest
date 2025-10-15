1f327b897267a19d7097ab55b155ff477a515bc4
Cesar Sanchez <byp3ngu1n@gmail.com>
2025-10-15 17:24:22 -0600

    test

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./src/vistas/img/favicon.ico">
+    <!--Font-Awesome-->
+    <link rel="stylesheet" href="./src/controlador/font-awesome/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/2321abd18a.js" crossorigin="anonymous"></script>
+    <!--Bootstrap 5.3 CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!--PWA-->
+    <meta name="MobileOptimized" content="width">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="comercio">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="apple-touch-icon" href="./src/vistas/img/logoCoyatocSA.png">
+    <link rel="apple-touch-startup-image" href="./src/vistas/img/logoCoyatocSA.png">
+    <link rel="manifest" href="./manifest.json">
+    <link rel="shortcut icon" type="image/svg" href="./src/vistas/img/logoCoyatocSA.png">
+    <style>
+        .td{
+            font-family:Arial, Helvetica, sans-serif;
+            font-size: small;
+        }
+    </style>
+
+
+    <title>Servicio Coyatoc</title>
+</head>
+<body>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12"><br>
+                    <ul class="nav navbar-expand-lg navbar-light bg-light">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#" style="color:#000000;">Iniciar Sesión</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="color:#000000;">Catálogo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Contactar con Ventas</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" style="color:#000000;" id="navbarDropdownMenuLink" data-toggle="dropdown">Sobre Nosotros</a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Nuestra Historia</a> <a class="dropdown-item" href="#">Misión</a> <a class="dropdown-item" href="#">Visión</a>
+                                <div class="dropdown-divider">
+                                </div> <a class="dropdown-item" href="#">Valores Organizacionales</a>
+                            </div>
+                        </li>
+                    </ul><br>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-1 text-center">
+                    <img src="./src/vistas/img/logoCoyatocSA.png" alt="Coyatoc">
+                </div>
+                <div class="col-md-11"><br>
+                <h4>Soporte y Reparación de Teléfonos Celulares</h4>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <img src="./src/vistas/img/repaCelular.jpeg" alt="repaCeluar" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <p><b>Dirección</b></p>
+                    <pre>Plaza de la Tecnología Local 12
+                        Av. Central Poniente 435.
+                        Tuxtla Gutiérrez, Chiapas</pre>
+                </div>
+                <div class="col-md-4 text-center">
+                    <p><b>Redes Sociales</b></p>
+                    <table class="default">
+                        <td class="fa-brands fa-facebook"><a href="#" style="font-family: Arial; color:#000000; text-decoration:none">&nbsp;&nbsp;CoyatocTGZ&nbsp;&nbsp;</a></td>
+                        <td class="fa-brands fa-x-twitter"><a href="#" style="font-family: Arial; color:#000000; text-decoration:none">&nbsp;@CoyatocTGZ&nbsp;</a></td>
+                        <td class="fa-brands fa-instagram"><a href="#" style="font-family: Arial; color:#000000; text-decoration:none">&nbsp;&nbsp;CoyatocTGZ&nbsp;</a></td>
+                        <td class="fa-brands fa-tiktok"><a href="#" style="font-family: Arial; color:#000000; text-decoration:none">&nbsp;CoyatocTGZ&nbsp;</a></td>
+                    </table>
+                </div>
+                <div class="col-md-4 text-center">
+                <p><b>Contáctanos</b></p>
+                    <table class="default">
+                        <td class="fa-brands fa-google"><a href="#" style="font-family: Arial; color:#000000; text-decoration:none">&nbsp;&nbsp;coyatoc@gmail.com&nbsp;&nbsp;</a></td>
+                        <td ></td>
+                        <td class="fa-solid fa-mobile-screen-button"><a href="#" style="font-family: Arial; color:#000000; text-decoration:none">&nbsp;&nbsp;961 61 54249</a></td>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--Bootstrap 4.6.2 JS-->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    <!--Bootstrap 5.3
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>-->
+</body>
+</html>
